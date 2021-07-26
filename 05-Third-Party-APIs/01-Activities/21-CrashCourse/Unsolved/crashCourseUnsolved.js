f8696bc346b106b8d2361aa161c4828a707e779a
Jenn Greiner <75277268+jenngreiner@users.noreply.github.com>
2021-07-26 14:07:41 -0600

    pushing course files

---
+++ 05-Third-Party-APIs/01-Activities/21-CrashCourse/Unsolved/crashCourseUnsolved.js
@@ -0,0 +1,75 @@
+// CRASH COURSE JS
+// ==========================================================
+
+// 1. BASIC VARIABLES
+// ==========================================================
+
+// Create a basic variable
+//*string
+var namex =
+  "Jennifer walked to the park and saw a dog and ran around the track";
+//*integer
+var number = 5;
+//*boolean
+var notABoolean = false;
+
+number = 7;
+// console.log(namex);
+// console.log(number);
+// console.log(notABoolean);
+
+// 2. ARRAYS // another type of variable
+// ==========================================================
+
+// Create an array of five strings
+// Create an array of five integers
+var numbers = [5, 2, 1, 0, 3];
+// Create an array of five booleans
+var booleans = [false, false, false, true, true];
+
+// console.log(breakfast);
+// console.log(numbers);
+// console.log(booleans);
+// 3. FOR LOOPS
+// ==========================================================
+var breakfast = ["waffles", "donuts", "eggs", "bacon", "oj"];
+// Create a for loop that loops through and console.log "My name is Bob" five times
+for (i = 0; 5 > i; i++) {
+  console.log("my name is bob");
+}
+
+// Create a for loop that loops through your five string array
+for (i = 0; 5 > i; i++) {
+  console.log(breakfast);
+}
+
+// 4. FUNCTIONS
+// ==========================================================
+
+// Create a function that takes two numbers and divides the first number by the second.
+// Then call that function
+
+// Create a function that takes in an array of numbers and then loops through the array and prints out numbers.
+// Then call that function
+
+// 5. OBJECTS
+// ==========================================================
+
+// Create a JavaScript Object
+
+// Console log any three of the properties in that object
+
+// Create an Array of 5 Objects
+
+// Console log 3 properties for every one of the five objects
+
+// 6. JQUERY EVENTS
+// ==========================================================
+
+// Create a basic html button then create an onClick event that triggers an on click event.
+
+// 7. OVERALL STRUCTURE
+// ==========================================================
+
+// Talk to student a little about the approach for "getting started" on an application.
+// Talk about the concept of variables, functions, calls.
---
+++ README.md
@@ -0,0 +1,3 @@
+# DU-DEN-FSF-PT-12-2020-U-C
+
+Look, I've changed :)
